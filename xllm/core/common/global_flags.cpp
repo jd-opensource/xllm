bfc7e6d685c80024dd522cab828bd7642a87f399
ext.yanwei25 <ext.yanwei25@jd.com>
2025-10-29 20:24:10 +0800

    feat: qwen3 dense support PreFetchWeight and IntraAddNorm

--- xllm/core/common/global_flags.cpp
+++ xllm/core/common/global_flags.cpp
@@ -389,3 +389,7 @@ DEFINE_string(reasoning_parser,
 
 // --- qwen3 reranker config ---
 DEFINE_bool(enable_qwen3_reranker, false, "Whether to enable qwen3 reranker.");
+
+DEFINE_bool(enable_prefetch_weight,
+            false,
+            "Whether to enable prefetch weight.");
