c6e8a1b261de2636470f31d53c5d7083a4fcf4a2
phantomlei <phantomlei3@gmail.com>
2025-11-04 15:20:01 +0800

    fix: fix header file format.

--- xllm/core/kernels/mlu/scaled_matmul.cpp
+++ xllm/core/kernels/mlu/scaled_matmul.cpp
@@ -13,7 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "glog/logging.h"
+#include <glog/logging.h>
+
 #include "mlu_ops_api.h"
 
 namespace xllm::kernel::mlu {
--- xllm/core/layers/common/indexer.cpp
+++ xllm/core/layers/common/indexer.cpp
@@ -12,14 +12,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-
 #include "indexer.h"
 
+#include <glog/logging.h>
 #include <torch/torch.h>
 
 #include <cmath>
 
-#include "glog/logging.h"
 #include "kernels/ops_api.h"
 
 namespace {
