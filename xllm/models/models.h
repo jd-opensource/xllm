59dac9c032d12611eb8e05eb539dd995aa76523b
wangziyue.28 <wangziyue.28@jd.com>
2025-11-17 16:37:53 +0800

    feat: support Qwen2-VL & GME-Qwen2-VL model on npu device.

--- xllm/models/models.h
+++ xllm/models/models.h
@@ -35,6 +35,8 @@ limitations under the License.
 #include "llm/qwen3_embedding.h"        // IWYU pragma: keep
 #include "vlm/minicpmv.h"               // IWYU pragma: keep
 #include "vlm/qwen2_5_vl.h"             // IWYU pragma: keep
+#include "vlm/qwen2_vl.h"               // IWYU pragma: keep
+#include "vlm/qwen2_vl_embedding.h"     // IWYU pragma: keep
 #include "vlm/qwen3_vl.h"               // IWYU pragma: keep
 #include "vlm/qwen3_vl_moe.h"           // IWYU pragma: keep
 #elif defined(USE_MLU)
