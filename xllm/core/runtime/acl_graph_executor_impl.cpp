81e4467f5ccaa855882117e5a83ac3f119f63dad
sanlio36 <1193789086@qq.com>
2026-02-04 07:38:42 +0000

    feat: support GLM-5 on npu device

--- xllm/core/runtime/acl_graph_executor_impl.cpp
+++ xllm/core/runtime/acl_graph_executor_impl.cpp
@@ -61,7 +61,8 @@ GraphPersistentParam::GraphPersistentParam(const ModelArgs& args,
       need_update_attn_mask_(need_update_attn_mask) {
   // Determine whether attention plan needs to be updated based on model type
   // Future logic can be extended here for more complex model-specific behavior
-  need_update_attention_plan_ = (args.model_type() != "deepseek_v32");
+  need_update_attention_plan_ =
+      (args.model_type() != "deepseek_v32" && args.model_type() != "glm5_moe");
 
   // Use max_tokens_per_batch for first dimension size
   // num_decode_tokens
