2d78c4fa1556b83f6c2ad484b5be5f887e6e8322
yq33victor <pengtao.156@jd.com>
2026-03-11 13:53:00 +0800

    refactor: remove the outdated glm5 model type.
    Signed-off-by: yq33victor <pengtao.156@jd.com>

--- xllm/xllm.cpp
+++ xllm/xllm.cpp
@@ -54,8 +54,7 @@ static std::unordered_set<std::string> deepseek_like_model_set = {
     "deepseek_v32_mtp",
     "kimi_k2",
     "glm4_moe_lite",
-    "glm5_moe",
-    "glm_moe_dsa",
+    "glm_moe_dsa",  // glm5 model type
     "joyai_llm_flash"};
 
 void shutdown_handler(int signal) {
