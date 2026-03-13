b20afa624e641f1de11d7c3c779700d02ada40f1
DragonFive <maxwell@hacari.com>
2026-03-13 11:13:31 +0800

    feat(rec): add OneRec foundation and runtime wiring without atb submodule dependency.

--- xllm/core/common/help_formatter.h
+++ xllm/core/common/help_formatter.h
@@ -91,9 +91,12 @@ const OptionCategory kXllmServiceOptions = {"XLLM-SERVICE OPTIONS",
 
 const OptionCategory kBeamSearchOptions = {"BEAM SEARCH OPTIONS",
                                            {"enable_beam_search_kernel",
-                                            "enable_rec_fast_sampler",
-                                            "enable_topk_sorted",
-                                            "output_rec_logprobs"}};
+                                            "enable_topk_sorted"}};
+
+const OptionCategory kRecOptions = {"REC OPTIONS",
+                                    {"enable_rec_fast_sampler",
+                                     "enable_rec_prefill_only",
+                                     "output_rec_logprobs"}};
 
 const OptionCategory kPrefixCacheOptions = {
     "PREFIX CACHE OPTIONS",
@@ -118,6 +121,7 @@ const std::vector<OptionCategory> kOptionCategories = {
     kMtpOptions,
     kXllmServiceOptions,
     kBeamSearchOptions,
+    kRecOptions,
     kPrefixCacheOptions,
     kOtherOptions};
 
