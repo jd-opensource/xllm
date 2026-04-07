8481efb9c4a05b4dca1bb1f508fb4f06a08f56bf
maxwell <maxwell@hacari.com>
2026-04-07 15:22:47 +0800

    feat: add onerec model implement[3/N]. (#1050)
    Co-authored-by: maxiaolong.maxwell <maxiaolong.maxwell@jd.com>

--- xllm/core/common/help_formatter.h
+++ xllm/core/common/help_formatter.h
@@ -98,11 +98,14 @@ const OptionCategory kXllmServiceOptions = {
     "XLLM-SERVICE OPTIONS",
     {"etcd_addr", "rank_tablefile", "etcd_namespace"}};
 
-const OptionCategory kBeamSearchOptions = {"BEAM SEARCH OPTIONS",
-                                           {"enable_beam_search_kernel",
-                                            "enable_rec_fast_sampler",
-                                            "enable_topk_sorted",
-                                            "output_rec_logprobs"}};
+const OptionCategory kBeamSearchOptions = {
+    "BEAM SEARCH OPTIONS",
+    {"enable_beam_search_kernel", "enable_topk_sorted"}};
+
+const OptionCategory kRecOptions = {"REC OPTIONS",
+                                    {"enable_rec_fast_sampler",
+                                     "enable_rec_prefill_only",
+                                     "output_rec_logprobs"}};
 
 const OptionCategory kPrefixCacheOptions = {
     "PREFIX CACHE OPTIONS",
@@ -128,6 +131,7 @@ const std::vector<OptionCategory> kOptionCategories = {
     kMtpOptions,
     kXllmServiceOptions,
     kBeamSearchOptions,
+    kRecOptions,
     kPrefixCacheOptions,
     kOtherOptions};
 
