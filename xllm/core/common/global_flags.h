627594e1abd7ad79f881263b83ee963466f1751d
shan-chen-feng <shanchenfeng@h-partners.com>
2026-04-08 19:40:18 +0800

    feat: support communication && computation overlap for QwenImageEditPlus pipeline.

--- xllm/core/common/global_flags.h
+++ xllm/core/common/global_flags.h
@@ -352,7 +352,7 @@ DECLARE_int64(dit_cache_start_blocks);
 
 DECLARE_int64(dit_cache_end_blocks);
 
-DECLARE_int64(dit_sp_communication_overlap);
+DECLARE_bool(dit_sp_communication_overlap);
 
 DECLARE_int64(dit_generation_image_area_max);
 
