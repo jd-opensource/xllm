de11ed278994454fc75db59315a36073bf339e40
Super User <root@A03-R40-I191-100-4100007.JD.LOCAL>
2026-06-09 16:52:04 +0800

    DiT module contains torch save and acc is successful!

--- xllm/core/framework/model/origin_model_args.h
+++ xllm/core/framework/model/origin_model_args.h
@@ -412,7 +412,7 @@ struct ModelArgs {
   PROPERTY(int32_t, max_window_layers) = 0;
 
   PROPERTY(int32_t, query_num) = 0;
-  PROPERTY(bool, encoder_embedding_mode) = false;
+  PROPERTY(bool, image_embedding_mode) = false;
 
   // number of speculative decoding tokens
   PROPERTY(int64_t, num_speculative_tokens) = 0;
@@ -457,18 +457,12 @@ struct ModelArgs {
   PROPERTY(double, vae_dropout) = 0.0;
   PROPERTY(bool, vae_is_residual) = false;
 
-  PROPERTY(float, batch_norm_eps) = 1e-04f;
-  PROPERTY(float, batch_norm_momentum) = 0.1f;
-  PROPERTY(std::vector<int64_t>, ae_patch_size) = {};
-
   // dit related args
   PROPERTY(int64_t, joint_attention_dim) = 0;
   PROPERTY(int64_t, pooled_projection_dim) = 0;
   PROPERTY(bool, guidance_embeds) = true;
   PROPERTY(std::vector<int64_t>, axes_dims_rope) = {};
   PROPERTY(int64_t, num_single_layers) = 0;
-
-  PROPERTY(float, mlp_ratio) = 3.0f;
   PROPERTY(int, timestep_guidance_channels) = 256;
   PROPERTY(double, eps) = 1e-6;
   PROPERTY(int64_t, patch_size) = 1;
@@ -579,7 +573,7 @@ inline bool has_linear_attention_layers(const ModelArgs& args) {
 
 inline std::ostream& operator<<(std::ostream& os, const ModelArgs& args) {
   os << "ModelArgs: [model_type: " << args.model_type();
-  os << ", encoder_embedding_mode: " << args.encoder_embedding_mode();
+  os << ", image_embedding_mode: " << args.image_embedding_mode();
   os << ", dtype: " << args.dtype();
   os << ", hidden_size: " << args.hidden_size();
   os << ", hidden_act: " << args.hidden_act();
